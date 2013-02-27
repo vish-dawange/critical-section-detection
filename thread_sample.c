da0264c5031ede63f0e129e79771bd2a1e5961f3
mj-jadhav <rajemayur.413@gmail.com>
2013-02-28 02:23:21 +0530

    Simple thread program which contains two threads with two global varibles and having some locking mechanism.

--- thread_sample.c
+++ thread_sample.c
@@ -5,33 +5,50 @@
 
 
 int X;
+int Y = 4;
 
 void* function1()
 {
-	while(1)
+    while(1)
 	{
-	X = 0; // write
-		printf("After thread ID A	writes to X, X = %d\n", X);
-		X++;
-		sleep(1);
+
+	    X = 0; // write
+
+	    printf("After thread ID A	writes to X, X = %d\n and Y = %d\n", X, Y);
+	    X++;
+
+	    sleep(1);
 	}
 }
 
 void* function2()
 {
-	while(1)
+    while(1)
 	{
-		X++;
-		printf("After thread ID B	reads from X, X = %d\n",	X);
-		sleep(1);
+	    sem_wait(&n);
+	    X++; Y++;
+	    sem_post(&n);
+	    printf("After thread ID B	reads from X, X = %d\n",	X);
+	    sleep(1);
+
 	}
 }
 
+int add()
+{
+    int X;
+    {
+	extern int X;
+	X++;
+    }
+    X++;
+}
 
 int main()
 {
-	void* status;
-	pthread_t thread1, thread2;
-	pthread_create(&thread1, &abc, function1, (void*) &thread_var);
-	pthread_create(&thread2, NULL, function2, NULL);
+    void* status;
+    pthread_t thread1, thread2;
+    add();
+    pthread_create(&thread1, NULL, function1, NULL);
+    pthread_create(&thread2, NULL, function2, NULL);
 }
