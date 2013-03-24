89c82cdc29f07cbca8bcf89b48f1b177850530ce
mj-jadhav <rajemayur.413@gmail.com>
2013-03-24 11:34:18 +0530

    Simple C program for finding suspected critical region caused by nested function calls.

--- Test Cases/nestfunc.c
+++ Test Cases/nestfunc.c
@@ -2,30 +2,39 @@
 #include<pthread.h>
 
 int x = 0;
-void foo()
+void *foo()
 {
-    x = 1;
+    	//x = 1;
 	printf("\nValue of x in ():%d",x);
 	printf("pthread");
-	//bar();
+	bar();
 }
 void bar()
 {
 	x = 2;
 	printf("\nValue of x in ():%d",x);
-	foo();
-	//foo_bar();
+	//foo();
+	foo_bar();
 }
 void foo_bar()
 {
 	x = 3;
 	printf("\nValue of x in r():%d",x);
-	bar();
+	//bar();
 		
 }
+
+void *func1()
+{
+	x++;
+}
+
 int main()
 {
-		foo_bar();
-		foo();
+		
+		pthread_create(&thread1,NULL,foo,NULL);
+		pthread_create(&thread1,NULL,func1,NULL);		
+			//foo_bar();
+		//foo();
 		return 0;
 }
